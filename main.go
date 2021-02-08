224cbd0307f4670d582f46d403444b980b7673e7
yerlanov <000117Gist>
2021-02-08 13:35:49 +0600

    added bin

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func (c *Conf) getSnmp() {
 	mibs := []string{"SNMPv2-MIB::sysUpTime.0",
 		"SNMPv2-MIB::sysDescr.0",
 		"IF-MIB::ifOperStatus.9",
-		"EhterLike-MIB::dot3StatsFCSErrors.9"}
+		"EtherLike-MIB::dot3StatsFCSErrors.9"}
 	oids := c.convertMibToOid(mibs)
 	result, err := params.Get(oids)
 	if err != nil {
@@ -90,7 +90,7 @@ func (c *Conf) convertMibToOid(mibs []string) []string {
 	mib.Debug = true
 	var oids []string
 
-	err := mib.LoadModules("IF-MIB", "SNMPv2-MIB", "EhterLike-MIB")
+	err := mib.LoadModules("IF-MIB", "SNMPv2-MIB", "EtherLike-MIB")
 	if err != nil {
 		log.Fatal(err)
 	}

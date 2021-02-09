fd1adf59acca8eb11ecb0661fb6ed667132a7323
yerlanov <000117Gist>
2021-02-09 09:24:54 +0600

    added bin

--- model/operStatus.go
+++ model/operStatus.go
@@ -21,7 +21,7 @@ func MapOperStatusToStruct(tkd []interface{}, agu []interface{}) OperStatus {
 		UpTimeTkd:        strconv.Itoa(int(tkd[0].(uint32))),
 		ModelTkd:         util.ConvertOctetStringToString(tkd[1]),
 		StatusClientPort: util.TranslateIfOperStatus(tkd[2]),
-		ErrorClientPort:  strconv.Itoa(int(tkd[3].(uint32))),
+		ErrorClientPort:  strconv.Itoa(int(tkd[3].(uint))),
 		MacAddressTkd:    util.ConvertDecimalToHexDecimal(tkd[4]),
 		UpTimeAgu:        strconv.Itoa(int(agu[0].(uint32))),
 		ModelAgu:         agu[1].(string),
